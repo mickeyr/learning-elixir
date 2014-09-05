dea967f6f1dbfc34c977d6c9311b3810fb92ccdd
Malcolm Roberts <maroberts@gmail.com>
2014-09-04 22:20:01 -0500

    Went all the way with the shortening

--- fizzbuzz2.ex
+++ fizzbuzz2.ex
@@ -1,7 +1,5 @@
 defmodule FizzBuzz do
-    def upto(n) when n>0 do
-        1..n |> Enum.map(&fizzbuzz/1)
-    end
+    def upto(n) when n>0, do: 1..n |> Enum.map(&fizzbuzz/1)
     
     defp fizzbuzz(n) when rem(n, 3) == 0 and rem(n, 5) == 0, do: "FizzBuzz"
     defp fizzbuzz(n) when rem(n, 3) == 0,                    do: "Fizz"
