faf1c5dffbf80eb0716714affd70cf3aed6e9483
Malcolm Roberts <maroberts@gmail.com>
2014-09-05 22:00:11 -0500

    Euler problem 3 added

--- project_euler.ex
+++ project_euler.ex
@@ -6,9 +6,18 @@ defmodule ProjectEuler do
         |> Enum.sum
     end
     
+    def problem_three() do
+        find_largest_prime_factor(600851475143, 2, 0)
+    end
+    
     defp fib_sequence(0), do: 0
     defp fib_sequence(1), do: 1
     defp fib_sequence(n), do: fib_sequence(n-2) + fib_sequence(n-1)
+    
+    ## Algorithm from http://math.stackexchange.com/questions/389675/largest-prime-factor-of-600851475143
+    defp find_largest_prime_factor(1, _, c), do: c
+    defp find_largest_prime_factor(a, b, c) when rem(a, b) == 0, do: find_largest_prime_factor(div(a, b), 2, max(b, c))
+    defp find_largest_prime_factor(a, b, c), do: find_largest_prime_factor(a, b+1, c)
 end
         
         
